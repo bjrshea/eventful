df72025cd350d891d17daf3a79a73e1ce5791ea4
Brendan Shea and Dan Mace <dan@>
2018-10-10 15:31:51 -0700

    so much

--- src/main.js
+++ src/main.js
@@ -25,13 +25,27 @@ $(document).ready(function() {
   "dataType": "json"
 }
 
-
+var color = "#46A7FF";
 
 $.ajax(settings).done(function (response) {
-  const firstEvent = new Event(response);
-  console.log(firstEvent.location);
-  var mymap = L.map('mapid').setView([firstEvent.location[0], firstEvent.location[1]], 13);
-  var marker = L.marker([firstEvent.location[0], firstEvent.location[1]]).addTo(mymap);
+  const todaysEvents = [];
+  const eventAPIArray = response.events.event;
+  eventAPIArray.forEach(function(event) {
+    const arrayItem = new Event(event);
+    todaysEvents.push(arrayItem);
+  })
+
+  var mymap = L.map('mapid').setView([todaysEvents[0].location[0], todaysEvents[0].location[1]], 13);
+    var counter = 1;
+  todaysEvents.forEach(function(event) {
+
+    $('.accordion').append('<div class="card-wrap"><div class="card"><div class="card-header" id="headingOne"><h5 class="mb-0"><button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapse' + counter + '" aria-expanded="true" aria-controls="collapseOne"><p>' + event.title + '</p><br><p>' + event.venueName + '</p></button></h5></div><div id="collapse' + counter + '" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample"><div class="card-body">' + event.cityName + ',&nbsp;' + event.regionName + '<br>' + event.startTime + '<br>' + event.description + '<br><a href="' + event.url + '">Buy Tickets</a></div></div></div></div>');
+
+    counter++;
+
+    var marker = L.marker([event.location[0], event.location[1]]).addTo(mymap);
+    marker.bindPopup("<span style='color: " + color + ";'><b>" + event.title + "</b></span><br><a target='_blank' href='" + event.venueUrl + "'>" + event.venueName + "</a>").openPopup();
+  });
 
   L.tileLayer(`https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}`, {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -40,16 +54,16 @@ $.ajax(settings).done(function (response) {
     accessToken: 'pk.eyJ1Ijoic3dhbXBjIiwiYSI6ImNqbjNtNDBhdDAwemUza293NmUzenB4aGwifQ.Nu7rHbRdFTIyA_iKow2ouA'
 }).addTo(mymap);
 
-  $('#title').text(firstEvent.title);
-  $('#venue a').text(firstEvent.venueName);
-  $('#venue a').attr("href", firstEvent.venueUrl);
-  $('#city').text(firstEvent.cityName);
-  $('#region').text(firstEvent.regionName);
-  // $('#date').
-  $('#time').text(firstEvent.startTime);
-  // $('.performers ul').append()
-  $('.description').html(firstEvent.description);
-  $('.ticketLink a').attr("href", firstEvent.url);
+  // $('#title').text(todaysEvents[0].title);
+  // $('#venue a').text(todaysEvents[0].venueName);
+  // $('#venue a').attr("href", todaysEvents[0].venueUrl);
+  // $('#city').text(todaysEvents[0].cityName);
+  // $('#region').text(todaysEvents[0].regionName);
+  // // $('#date').
+  // $('#time').text(todaysEvents[0].startTime);
+  // // $('.performers ul').append()
+  // $('.description').html(todaysEvents[0].description);
+  // $('.ticketLink a').attr("href", todaysEvents[0].url);
 
 });
 });
